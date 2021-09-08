94f378d31b63d2c37f466041911750650ab2d18d
toby <tobywang2021@gmail.com>
2021-09-08 14:11:53 +0800

    add app02 init

---
+++ Tools/Web/React/Day-01/app-02/index.js
@@ -0,0 +1,9 @@
+// import 文件
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// 創建文件
+let element = React.createElement('h1', null, 'Hello React!! I am toby.')
+
+// render 文件
+ReactDOM.render(element, document.getElementById('root'))
---
+++ Tools/Web/React/Day-01/app-02/readme.md
@@ -0,0 +1,6 @@
+# install package and init application
+```
+npx create-react-app app-name
+```
+# Go to app-name -> src -> index.js
+begin your application.
