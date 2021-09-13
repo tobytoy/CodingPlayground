766350626def0b564d554c7a42daa1ce013c185d
toby <tobywang2021@gmail.com>
2021-09-13 11:29:28 +0800

    rabbit add default user

--- Tools/environment/playground/packages/begin/rabbit.sh
+++ Tools/environment/playground/packages/begin/rabbit.sh
@@ -1,6 +1,7 @@
 #! /bin/sh                                                                                                                                                                                    
 
-rabbitmqctl add_user toby 1234
-rabbitmqctl set_user_tags toby administrator
-rabbitmqctl set_permissions -p / toby ".*" ".*" ".*"
-rabbitmqctl  delete_user guest
+# rabbitmqctl add_user toby toby
+# rabbitmqctl set_user_tags toby administrator
+# rabbitmqctl set_permissions -p / toby ".*" ".*" ".*"
+# rabbitmqctl  delete_user guest
+
